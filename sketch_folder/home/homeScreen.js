b784e1ecf2533889107c540e6b48e807acfa64bd
Fabio <totalmentecasuale@users.noreply.github.com>
2020-03-26 14:08:43 +0100

    Fixed HTML continuous creation bug
    Removed unnecessary statements

--- sketch_folder/home/homeScreen.js
+++ sketch_folder/home/homeScreen.js
@@ -12,26 +12,23 @@ class gameButton {
   }
 
   render(){
-    //edit with graphics-----------------
-    imageMode(CENTER);
-    image(this.butt, this.posX, this.posY);
-    //-----------------------------------
+    this.butt.position(this.posX - this.bWidth/2, this.posY - this.bHeight/2);
     if(this.isOver()){
       this.popup.show(this.posX, this.posY, this.bWidth);
     }
   }
 
   update(){
-    //Just in case we want them to move aside
     this.posX = width/2;
     this.posY = lerp(height * 0.15, height, 0.2 * this.ord);
     this.bWidth = max(this.bWidth, width * 0.2);
     this.bHeight = height * 0.09;
-    this.butt = createGraphics(this.bWidth, this.bHeight);
-    //edit with p5.Graphics-------------
+    this.butt.size(this.bWidth, this.bHeight);
+
     this.butt.noStroke();
     this.butt.fill(colCedarChest);
     this.butt.rect(0, 0, this.bWidth, this.bHeight, 30, 30, 30, 30);
+
     this.butt.fill(colGainsboro);
     this.butt.stroke(colJet);
     this.butt.strokeWeight(2);
@@ -39,11 +36,6 @@ class gameButton {
     this.butt.textSize(fontsize);
     this.butt.textAlign(CENTER, CENTER);
     this.butt.text(this.bText, this.butt.width/2, this.butt.height/2);
-    var divName = "."+this.id;
-    var curDiv = selectAll("#",this.id);
-    //curDiv.position(this.posX, this.posY);
-    // this.butt.parent(this.id); this function doesn't work
-    //-----------------------------------
   }
 
   show(){
