a15d1f3f12e4da1bef42effe5abf9c3728354d57
Giorgio De Luca <delucagiorgiote@gmail.com>
2020-03-28 18:19:03 +0100

    fix server. finally deploy on HEROKU works! :)

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+var express = require('express')
+var path = require('path')
+var cors = require('cors');
+var serveStatic = require('serve-static')
+var favicon = require('serve-favicon');
+
+
+var app = express()
+var distDir = __dirname;
+app.use(express.static(distDir));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+
+
+app.options('*', cors());
+app.use(favicon('favicon.ico'));
+
+app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(cors({
+  origin: 'https://catchthepitch.herokuapp.com:8080',
+  credentials: true
+}));
+
+
+var port = process.env.PORT || 8000
+app.listen(port)
+console.log('server started ' + port)
+
